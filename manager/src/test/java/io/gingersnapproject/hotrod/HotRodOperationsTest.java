21142a2273d2791f98c0a3704618f8f78a75aea9
jabolina <jbolina@redhat.com>
2023-01-27 16:15:13 -0300

    Create sample test

--- manager/src/test/java/io/gingersnapproject/hotrod/HotRodOperationsTest.java
+++ manager/src/test/java/io/gingersnapproject/hotrod/HotRodOperationsTest.java
@@ -3,6 +3,12 @@ package io.gingersnapproject.hotrod;
 import io.gingersnapproject.database.DatabaseResourcesLifecyleManager;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.collection.IsCollectionWithSize;
+import org.hamcrest.collection.IsIterableContainingInAnyOrder;
+import org.hamcrest.collection.IsIterableWithSize;
 import org.infinispan.client.hotrod.DataFormat;
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
@@ -11,10 +17,13 @@ import org.infinispan.commons.dataconversion.MediaType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.shaded.org.hamcrest.Matchers;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @QuarkusTest
@@ -58,5 +67,34 @@ public class HotRodOperationsTest {
       cache.put("key2", "value3");
 
       assertThat(cache.getAll(values.keySet())).isEqualTo(values);
+   }
+
+   @Test
+   public void putAndDelete() {
+      cache.put("key1", "value1");
+      cache.put("key2", "value2");
+
+      assertThat(cache.get("key1")).isEqualTo("value1");
+      assertThat(cache.get("key2")).isEqualTo("value2");
+
+      given()
+            .when().get("/rules/" + RULE_NAME)
+            .then()
+            .statusCode(200)
+            .body("", IsCollectionWithSize.hasSize(2))
+            .body("", IsIterableContainingInAnyOrder.containsInAnyOrder("key1", "key2"));
+
+
+      cache.remove("key2");
+
+      assertThat(cache.get("key1")).isEqualTo("value1");
+      assertThat(cache.get("key2")).isNull();
+
+      given()
+            .when().get("/rules/" + RULE_NAME)
+            .then()
+            .statusCode(200)
+            .body("", IsCollectionWithSize.hasSize(1))
+            .body("", IsIterableContainingInAnyOrder.containsInAnyOrder("key1"));
    }
 }
